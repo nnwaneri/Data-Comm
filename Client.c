738ce2076e35d7250a8c460a1968b4040370b35c
nnwaneri <31398041+nnwaneri@users.noreply.github.com>
2018-10-18 22:26:40 -0400

    updated client.c

--- Client.c
+++ Client.c
@@ -6,64 +6,39 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include<ctype.h>
+#include <ctype.h>
 
-void error(const char *message){
-    perror(msg);
-    exit(0);
-}
 int main(int argc, char *argv[]){
     printf("----------------------------------------\n");
     printf("Data Communication & Network Programming\n");
     printf("Norah Nwaneri\n");
     printf("----------------------------------------\n");
-    int sockfd, portno, n;
-    struct socketaddy_in server_addy;
-    struct hostent *server;
-    char buffer[512];
-    if (argc < 3){
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        exit(0);
+    int sid,n;
+    char buffer[1024];
+    struct sockaddr_in saddr;
+    struct hostent *hen;
+    if(argc<3){
+        fprintf(stderr,”Error: Host name and port is not given \n”);
+        exit(1);
     }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR socket will not open.");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, host does not exists.\n");
-        exit(0);
+    sid=socket(AF_INET,SOCK_DGRAM,0);
+    if(sid<0)
+        perror("Socket create");
+    hen=gethostbyname(argv[1]);
+    if(hen==NULL){
+        fprintf(stdout,"Host not found");
+        exit(1);
     }
-    bzero((char *) &server_addy, sizeof(server_addy));
-    server_addy.sin_family = AF_INET;
-    bcopy((char *)server->h_addy,
-          (char *)&server_addy.sin_addy.s_addy,
-          server->h_length);
-    server_addy.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddy *) &server_addy,sizeof(server_addy)) < 0)
-        error("ERROR connecting to server.");
-    
-    bzero(buffer,512);
-    
-    FILE *f;
-    
-    int words = 0;
-    char c;
-    f=fopen("sample.txt","r");
-    while((c=getc(f))!= EOF){
-        fscanf(f , "%s" , buffer);
-        if(isspace(c)||c=='\t')
-            words++;
-    }
-    write(sockfd, &words, sizeof(int));
-    rewind(f);
-    char ch ;
-    while(ch != EOF){
-        fscanf(f , "%s" , buffer);
-        write(sockfd,buffer,512);
-        ch = fgetc(f);
-    }
-    printf("The file was sent.");
-    close(sockfd);
-    return 0;
+    saddr.sin_family=AF_INET;
+    saddr.sin_port=htons(atoi(argv[2]));
+    bcopy((char *)hen->h_addr,(char *)&saddr.sin_addr.s_addr,hen->h_length);
+    if(bind(sid,(struct sockaddr *)&saddr,sizeof(saddr))<0) 
+        perror("Socket_bind");
+    printf("Enter the data you want to send : \n");
+    fgets(buffer,1023,stdin);
+    n=sendto(sid,buffer,1023,0,(struct sockaddr *)&saddr,sizeof(saddr));
+    if(n<0) 
+        perror(“Error in send to”);
+    close(sid);
+    return(0);
 }
